a31bfa9739960b1d57a4fbe8c7d2ca7552defd9e
npwoods <npwoods@mess.org>
2019-05-29 07:48:35 -0400

    Another validity check

--- src/validity.cpp
+++ src/validity.cpp
@@ -25,9 +25,21 @@ bool validity_checks()
 
 	{
 		auto result = util::string_split(std::wstring(L"Alpha,Bravo,Charlie"), [](wchar_t ch) { return ch == ','; });
-		assert(result[0] == "Alpha");
-		assert(result[1] == "Bravo");
-		assert(result[2] == "Charlie");
+		assert(result[0] == L"Alpha");
+		assert(result[1] == L"Bravo");
+		assert(result[2] == L"Charlie");
+	}
+
+	{
+		std::vector<std::string> vec = { "Alpha", "Bravo", "Charlie" };
+		std::string result = util::string_join(std::string(","), vec);
+		assert(result == "Alpha,Bravo,Charlie");
+	}
+
+	{
+		std::vector<std::wstring> vec = { L"Alpha", L"Bravo", L"Charlie" };
+		std::wstring result = util::string_join(std::wstring(L","), vec);
+		assert(result == L"Alpha,Bravo,Charlie");
 	}
 
 	return true;
