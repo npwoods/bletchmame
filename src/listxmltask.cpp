f41601e50b75a14394410cbb674d89ee6f010a98
npwoods <npwoods@mess.org>
2019-05-31 20:54:59 -0400

    Accessing machine list with -listxmllight, optimized reading the XML

--- src/listxmltask.cpp
+++ src/listxmltask.cpp
@@ -18,29 +18,62 @@
 
 namespace
 {
+	class BlockingInputStream : public wxInputStream
+	{
+	public:
+		BlockingInputStream(wxInputStream &input)
+			: m_input(input)
+		{
+		}
+
+	protected:
+		virtual size_t OnSysRead(void *buffer, size_t bufsize)
+		{
+			return InternalOnSysRead(static_cast<uint8_t *>(buffer), bufsize);
+		}
+
+	private:
+		wxInputStream & m_input;
+
+		size_t InternalOnSysRead(uint8_t *buffer, size_t bufsize)
+		{
+			bool done = false;
+			size_t position = 0;
+
+			while (!done && position < bufsize)
+			{
+				m_input.Read(&buffer[position], bufsize - position);
+				position += m_input.LastRead();
+				done = m_input.LastRead() == 0;
+			}
+
+			return position;
+		}
+
+	};
+
 	class ListXmlTask : public Task
 	{
 	public:
 		virtual std::vector<wxString> GetArguments(const Preferences &) const
 		{
-			return { "-listxml", "-nodtd" };
+			return { "-listxmllight", "-nodtd" };
 		}
 
 		virtual void Process(wxProcess &process, wxEvtHandler &handler) override;
 
-    private:
-        ListXmlResult InternalProcess(wxInputStream &input);
-        Machine MachineFromXmlNode(wxXmlNode &node);
+	private:
+		ListXmlResult InternalProcess(wxInputStream &input);
+		Machine MachineFromXmlNode(wxXmlNode &node);
 
-        bool LoadXml(wxXmlDocument &xml, wxInputStream &input)
-        {
-            // wxXmlDocument::Load() seems to interpret a stream that returns less than the expected amount of
-            // bytes as a sign of being done.  We're using a wxBufferedInputStream to get around this behavior
-            wxBufferedInputStream buffered_stream(input);
-            return xml.Load(buffered_stream);
-        }
-    };
-
+		bool LoadXml(wxXmlDocument &xml, wxInputStream &input)
+		{
+			// wxXmlDocument::Load() seems to interpret a stream that returns less than the expected amount of
+			// bytes as a sign of being done.  We're using a wxBufferedInputStream to get around this behavior
+			BlockingInputStream blocking_stream(input);
+			return xml.Load(blocking_stream);
+		}
+	};
 }
 
 //**************************************************************************
