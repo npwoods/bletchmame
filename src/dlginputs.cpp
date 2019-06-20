f5e3ef23cd3dca2fcd11e6ea2a58926ec9e5f45a
npwoods <npwoods@mess.org>
2019-06-20 18:52:32 -0400

    Inputs dialog is now scrolling

--- src/dlginputs.cpp
+++ src/dlginputs.cpp
@@ -9,6 +9,7 @@
 #include <wx/dialog.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
+#include <wx/scrolwin.h>
 #include <wx/button.h>
 
 #include "dlginputs.h"
@@ -27,7 +28,7 @@ namespace
 		InputsDialog(wxWindow &parent, const std::vector<Input> &inputs);
 
 	private:
-		template<typename TControl, typename... TArgs> TControl &AddControl(wxSizer &sizer, int flags, TArgs&&... args);
+		template<typename TControl, typename... TArgs> TControl &AddControl(wxWindow &parent, wxSizer &sizer, int proportion, int flags, TArgs&&... args);
 	};
 };
 
@@ -43,14 +44,26 @@ namespace
 InputsDialog::InputsDialog(wxWindow &parent, const std::vector<Input> &inputs)
 	: wxDialog(&parent, wxID_ANY, "Inputs", wxDefaultPosition, wxDefaultSize, wxCAPTION | wxSYSTEM_MENU | wxCLOSE_BOX | wxRESIZE_BORDER)
 {
-	wxString buffer;
 	int id = wxID_LAST + 1;
+
+	// scrolled view
+	std::unique_ptr<wxBoxSizer> outer_sizer = std::make_unique<wxBoxSizer>(wxVERTICAL);
+	wxScrolledWindow &scrolled = AddControl<wxScrolledWindow>(*this, *outer_sizer, 1, wxALL | wxEXPAND);
+
+	// buttons
+	std::unique_ptr<wxBoxSizer> button_sizer = std::make_unique<wxBoxSizer>(wxHORIZONTAL);
+	wxButton &restore_button	= AddControl<wxButton>(*this, *button_sizer, 0, wxALL, id++, "Restore Defaults");
+	wxButton &ok_button			= AddControl<wxButton>(*this, *button_sizer, 0, wxALL, wxID_OK, "OK");
+	outer_sizer->Add(button_sizer.release(), 0, wxALIGN_RIGHT);
+	
+	// restore button is not yet implemented
+	restore_button.Disable();
 
 	// main grid
 	std::unique_ptr<wxFlexGridSizer> grid_sizer = std::make_unique<wxFlexGridSizer>(2);
-	grid_sizer->AddGrowableCol(1);
 
 	// build controls
+	wxString buffer;
 	for (const Input &input : inputs)
 	{
 		for (const InputSeq &input_seq : input.m_seqs)
@@ -73,12 +86,21 @@ InputsDialog::InputsDialog(wxWindow &parent, const std::vector<Input> &inputs)
 				throw false;
 			}
 
-			AddControl<wxButton>(*grid_sizer, wxALL | wxEXPAND, id++, *name);
-			AddControl<wxStaticText>(*grid_sizer, wxALL, id++, input_seq.m_text);
+			AddControl<wxButton>(scrolled, *grid_sizer, 0, wxALL | wxEXPAND, id++, *name);
+			AddControl<wxStaticText>(scrolled, *grid_sizer, 0, wxALL, id++, input_seq.m_text);
 		}
 	}
 
-	SetSizerAndFit(grid_sizer.release());
+	// configure scrolling
+	scrolled.SetScrollRate(25, 25);
+	scrolled.SetSizer(grid_sizer.release());
+	scrolled.FitInside();
+
+	// finally set the outer sizer
+	SetSizer(outer_sizer.release());
+
+	// appease compiler
+	(void)ok_button;
 }
 
 
@@ -87,10 +109,10 @@ InputsDialog::InputsDialog(wxWindow &parent, const std::vector<Input> &inputs)
 //-------------------------------------------------
 
 template<typename TControl, typename... TArgs>
-TControl &InputsDialog::AddControl(wxSizer &sizer, int flags, TArgs&&... args)
+TControl &InputsDialog::AddControl(wxWindow &parent, wxSizer &sizer, int proportion, int flags, TArgs&&... args)
 {
-	TControl *control = new TControl(this, std::forward<TArgs>(args)...);
-	sizer.Add(control, 0, flags, 4);
+	TControl *control = new TControl(&parent, std::forward<TArgs>(args)...);
+	sizer.Add(control, proportion, flags, 4);
 	return *control;
 }
 
