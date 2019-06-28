37a00d52887438ae1b0a292f346b9207ef3e1db4
npwoods <npwoods@mess.org>
2019-06-27 20:19:16 -0400

    Info code cleanup

--- src/info.cpp
+++ src/info.cpp
@@ -43,44 +43,44 @@ bool info::database::load(const wxString &file_name)
 		return false;
 
 	// check the header
-	const info::binaries::header &header(this->header());
-	if (m_data.size() < sizeof(info::binaries::header))
+	const binaries::header &hdr(header());
+	if (m_data.size() < sizeof(binaries::header))
 		return false;
-	if (header.m_magic != info::binaries::MAGIC_HEADER)
+	if (hdr.m_magic != binaries::MAGIC_HEADER)
 		return false;
-	if (header.m_version != info::binaries::VERSION)
+	if (hdr.m_version != binaries::VERSION)
 		return false;
 
 	// offsets
-	size_t machines_offset					= sizeof(info::binaries::header);
-	size_t devices_offset					= machines_offset					+ (header.m_machines_count					* sizeof(info::binaries::machine));
-	size_t configurations_offset			= devices_offset					+ (header.m_devices_count					* sizeof(info::binaries::device));
-	size_t configuration_settings_offset	= configurations_offset				+ (header.m_configurations_count			* sizeof(info::binaries::configuration));
-	size_t configuration_conditions_offset	= configuration_settings_offset		+ (header.m_configuration_settings_count	* sizeof(info::binaries::configuration_setting));
-	size_t string_table_offset				= configuration_conditions_offset	+ (header.m_configuration_conditions_count	* sizeof(info::binaries::configuration_condition));
+	size_t machines_offset					= sizeof(binaries::header);
+	size_t devices_offset					= machines_offset					+ (hdr.m_machines_count					* sizeof(binaries::machine));
+	size_t configurations_offset			= devices_offset					+ (hdr.m_devices_count					* sizeof(binaries::device));
+	size_t configuration_settings_offset	= configurations_offset				+ (hdr.m_configurations_count			* sizeof(binaries::configuration));
+	size_t configuration_conditions_offset	= configuration_settings_offset		+ (hdr.m_configuration_settings_count	* sizeof(binaries::configuration_setting));
+	size_t string_table_offset				= configuration_conditions_offset	+ (hdr.m_configuration_conditions_count	* sizeof(binaries::configuration_condition));
 
 	// sanity check the string table
 	if (m_data.size() < string_table_offset + 1)
 		return false;
 	if (m_data[string_table_offset] != '\0')
 		return false;
-	if (!unaligned_check(&m_data[string_table_offset + 1], info::binaries::MAGIC_STRINGTABLE_BEGIN))
+	if (!unaligned_check(&m_data[string_table_offset + 1], binaries::MAGIC_STRINGTABLE_BEGIN))
 		return false;
-	if (m_data[m_data.size() - sizeof(info::binaries::MAGIC_STRINGTABLE_END) - 1] != '\0')
+	if (m_data[m_data.size() - sizeof(binaries::MAGIC_STRINGTABLE_END) - 1] != '\0')
 		return false;
-	if (!unaligned_check(&m_data[m_data.size() - sizeof(info::binaries::MAGIC_STRINGTABLE_END)], info::binaries::MAGIC_STRINGTABLE_END))
+	if (!unaligned_check(&m_data[m_data.size() - sizeof(binaries::MAGIC_STRINGTABLE_END)], binaries::MAGIC_STRINGTABLE_END))
 		return false;
 
 	// things look good, set up our tables
-	m_view_machines	= decltype(m_view_machines)(*this,	machines_offset,	header.m_machines_count);
-	m_view_devices	= decltype(m_view_devices)(*this,	devices_offset,		header.m_devices_count);
+	m_view_machines	= decltype(m_view_machines)(*this,	machines_offset, hdr.m_machines_count);
+	m_view_devices	= decltype(m_view_devices)(*this,	devices_offset, hdr.m_devices_count);
 
 	// ...and set up string table info
 	m_loaded_strings.clear();
 	m_string_table_offset = string_table_offset;
 
 	// ...and last but not least set up the version
-	m_version = &get_string(header.m_build_strindex);
+	m_version = &get_string(hdr.m_build_strindex);
 	return true;
 }
 
