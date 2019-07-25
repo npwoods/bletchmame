173db0cb9fa587d94704e11be8388696b4007efe
npwoods <npwoods@mess.org>
2019-07-25 18:13:32 -0400

    Update make-msi.bat

--- make-msi.bat
+++ make-msi.bat
@@ -1,3 +1,5 @@
+dir "C:\Program Files (x86)"
+echo $PATH
 candle.exe -v bletchmame.wxs -out bletchmame.wixobj > candle.err
 light.exe -v -ext WixUIExtension bletchmame.wixobj -out BletchMAME.msi > light.err
 dir
