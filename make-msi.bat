ac67c55d6f0face3ff1d5d719117ef3aea01b37e
npwoods <npwoods@mess.org>
2019-07-25 17:58:51 -0400

    tweaks

--- make-msi.bat
+++ make-msi.bat
@@ -1,3 +1,5 @@
-candle -v bletchmame.wxs -out bletchmame.wixobj
-light -v -ext WixUIExtension bletchmame.wixobj -out BletchMAME.msi
+candle.exe -v bletchmame.wxs -out bletchmame.wixobj > candle.err
+light.exe -v -ext WixUIExtension bletchmame.wixobj -out BletchMAME.msi > light.err
 dir
+type candle.err
+type light.err
