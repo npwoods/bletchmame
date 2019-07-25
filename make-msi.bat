e4f6fc1ccdb843cdafad9800b06de522c825e69e
npwoods <npwoods@mess.org>
2019-07-25 18:19:23 -0400

    Update make-msi.bat

--- make-msi.bat
+++ make-msi.bat
@@ -1,5 +1,7 @@
-dir "C:\Program Files (x86)"
-echo $PATH
+set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.10\
+set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\
+which candle.exe
+which light.exe
 candle.exe -v bletchmame.wxs -out bletchmame.wixobj > candle.err
 light.exe -v -ext WixUIExtension bletchmame.wixobj -out BletchMAME.msi > light.err
 dir
