6b0622091f018088f4651c42a2be07bea11dde0e
npwoods <npwoods@mess.org>
2019-07-25 20:01:13 -0400

    Finally have this working...

--- make-msi-wix310.bat
+++
@@ -1,4 +0,0 @@
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" -help
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -help
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\candle.exe" -v bletchmame.wxs -out bletchmame.wixobj
-"c:\Program Files (x86)\WiX Toolset v3.10\bin\light.exe" -v -ext WixUIExtension bletchmame.wixobj -out BletchMAME.msi > light.err
