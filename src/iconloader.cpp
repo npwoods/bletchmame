5c3f30c005954d3aa8193a8e31300916707c062b
npwoods <npwoods@mess.org>
2019-11-10 11:31:47 -0500

    Loading icons out of ZIP files on demand, speeding things up

--- src/iconloader.cpp
+++ src/iconloader.cpp
@@ -7,8 +7,6 @@
 ***************************************************************************/
 
 #include <wx/icon.h>
-#include <wx/zipstrm.h>
-#include <wx/wfstream.h>
 
 #include "iconloader.h"
 #include "prefs.h"
@@ -47,6 +45,7 @@ void IconLoader::RefreshIcons()
 	// clear ourselves out
 	m_image_list.RemoveAll();
 	m_icon_map.clear();
+	m_zip_files.clear();
 
 	// loop through all icon paths
 	std::vector<wxString> paths = m_prefs.GetSplitPaths(Preferences::path_type::icons);
@@ -64,66 +63,21 @@ void IconLoader::RefreshIcons()
 
 bool IconLoader::LoadIconsFromZipFile(const wxString &zip_file_name)
 {
-	// first open the file
-	wxFileInputStream input(zip_file_name);
-	if (!input.IsOk())
-		return false;
-
-	// then open as a zip
-	wxZipInputStream zip(input);
-	if (!zip.IsOk())
-		return false;
-
-	// ok now we have a real zip file; get down to business
-	wxZipEntry *entry;
-	const wxString icon_extension = wxT(".ico");
-
-	// get the next entry
-	while ((entry = zip.GetNextEntry()) != nullptr)
+	// filter to sanity check ZIP file entries
+	auto filter = [](const wxZipEntry &entry)
 	{
-		// sanity check the entry
-		if (!entry->IsDir() && entry->GetSize() > 0 && entry->GetSize() < 1000000)
-		{
-			// is this a *.ico file?
-			wxString name = entry->GetName();
-			if (name.EndsWith(icon_extension))
-			{
-				// if so, get the real name
-				name.resize(name.size() - icon_extension.size());
+		return entry.GetName().EndsWith(wxT(".ico"))
+			&& entry.GetSize() > 0
+			&& entry.GetSize() < 1000000;
+	};
 
-				// open up the ZIP entry
-				zip.OpenEntry(*entry);
-
-				// and load it!
-				LoadIcon(std::move(name), zip);
-			}
-		}
-	}
-	return true;
-}
-
-
-//-------------------------------------------------
-//  LoadIcon
-//-------------------------------------------------
-
-bool IconLoader::LoadIcon(wxString &&icon_name, wxInputStream &stream)
-{
-	// load the icon into a file
-	wxImage image;
-	if (!image.LoadFile(stream, wxBITMAP_TYPE_ICO))
+	// open up the zip file
+	ZipFile::ptr zip = std::make_unique<ZipFile>(zip_file_name, filter);
+	if (zip->EntryCount() <= 0)
 		return false;
 
-	// rescale it
-	wxImage rescaled_image = image.Rescale(ICON_SIZE_X, ICON_SIZE_Y);
-	
-	// and add it to the image list
-	int icon_number = m_image_list.Add(rescaled_image);
-	if (icon_number < 0)
-		return false;
-
-	// we have an icon!  add it to the map
-	m_icon_map.emplace(std::move(icon_name), icon_number);
+	// success - push this back
+	m_zip_files.push_back(std::move(zip));
 	return true;
 }
 
@@ -134,8 +88,105 @@ bool IconLoader::LoadIcon(wxString &&icon_name, wxInputStream &stream)
 
 int IconLoader::GetIcon(const wxString &icon_name)
 {
+	int result = -1;
 	auto iter = m_icon_map.find(icon_name);
-	return iter != m_icon_map.end()
-		? iter->second
-		: -1;
+	if (iter != m_icon_map.end())
+	{
+		// we've previously loaded (or tried to load) this icon; use our old result
+		result = iter->second;
+	}
+	else
+	{
+		// we have not tried to load this icon - first determine the real file name
+		wxString icon_file_name = icon_name + wxT(".ico");
+
+		// and try to load it from each ZIP file
+		for (const auto &zip_file : m_zip_files)
+		{
+			wxInputStream *stream = zip_file->OpenFile(icon_file_name);
+			if (stream)
+			{
+				// we've found an entry - try to load the icon; note that while this can
+				// fail, we want to memoize the failure
+				result = LoadIcon(std::move(icon_file_name), *stream);
+
+				// record the result in the icon map
+				m_icon_map.emplace(icon_name, result);
+				break;
+			}
+		}
+	}
+	return result;
+}
+
+
+//-------------------------------------------------
+//  LoadIcon
+//-------------------------------------------------
+
+int IconLoader::LoadIcon(wxString &&icon_name, wxInputStream &stream)
+{
+	// load the icon into a file
+	wxImage image;
+	if (!image.LoadFile(stream, wxBITMAP_TYPE_ICO))
+		return -1;
+
+	// rescale it
+	wxImage rescaled_image = image.Rescale(ICON_SIZE_X, ICON_SIZE_Y);
+	
+	// and add it to the image list
+	int icon_number = m_image_list.Add(rescaled_image);
+	if (icon_number < 0)
+		return -1;
+
+	// we have an icon!  add it to the map
+	m_icon_map.emplace(std::move(icon_name), icon_number);
+	return icon_number;
+}
+
+
+//-------------------------------------------------
+//  ZipFile ctor
+//-------------------------------------------------
+
+template<class TFilter>
+IconLoader::ZipFile::ZipFile(const wxString &zip_filename, TFilter filter)
+	: m_file_stream(zip_filename)
+	, m_zip_stream(m_file_stream)
+{
+	if (m_file_stream.IsOk() && m_zip_stream.IsOk())
+	{
+		wxZipEntry *entry;
+		while ((entry = m_zip_stream.GetNextEntry()) != nullptr)
+		{
+			if (!entry->IsDir() && filter(*entry))
+				m_entries.emplace(entry->GetName(), *entry);
+		}
+	}
+}
+
+
+//-------------------------------------------------
+//  ZipFile::EntryCount
+//-------------------------------------------------
+
+size_t IconLoader::ZipFile::EntryCount() const
+{
+	return m_entries.size();
+}
+
+
+//-------------------------------------------------
+//  ZipFile::OpenFile
+//-------------------------------------------------
+
+wxInputStream *IconLoader::ZipFile::OpenFile(const wxString &filename)
+{
+	// find the entry
+	auto iter = m_entries.find(filename);
+
+	// if we found it, try to open it
+	return iter != m_entries.end() && m_zip_stream.OpenEntry(iter->second)
+		? &m_zip_stream
+		: nullptr;
 }
--- src/iconloader.h
+++ src/iconloader.h
@@ -11,8 +11,11 @@
 #ifndef ICONLOADER_H
 #define ICONLOADER_H
 
+#include <memory>
 #include <unordered_map>
 #include <wx/imaglist.h>
+#include <wx/zipstrm.h>
+#include <wx/wfstream.h>
 
 class Preferences;
 
@@ -30,12 +33,33 @@ public:
 	int GetIcon(const wxString &icon_name);
 
 private:
+	class ZipFile
+	{
+	public:
+		typedef std::unique_ptr<ZipFile> ptr;
+
+		// ctors
+		template<class TFilter> ZipFile(const wxString &zip_filename, TFilter filter);
+		ZipFile(const ZipFile &) = delete;
+		ZipFile(ZipFile &&) = delete;
+
+		// methods
+		size_t EntryCount() const;
+		wxInputStream *OpenFile(const wxString &filename);
+
+	private:
+		wxFileInputStream							m_file_stream;
+		wxZipInputStream							m_zip_stream;
+		std::unordered_map<wxString, wxZipEntry>	m_entries;
+	};
+
 	Preferences &						m_prefs;
 	wxImageList							m_image_list;
 	std::unordered_map<wxString, int>	m_icon_map;
+	std::vector<ZipFile::ptr>			m_zip_files;
 
 	bool LoadIconsFromZipFile(const wxString &zip_file_name);
-	bool LoadIcon(wxString &&icon_name, wxInputStream &stream);
+	int LoadIcon(wxString &&icon_name, wxInputStream &stream);
 };
 
 #endif // ICONLOADER_H
