3e06d459fe4188d7a9ffb35e0e7648f2e7183b72
npwoods <npwoods@mess.org>
2020-08-08 23:24:04 -0400

    Ok, this is absurd

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -66,8 +66,11 @@ int main(int argc, char *argv[])
     }
 
     if (anyFailed)
+    {
         std::cout << "TEST FAILURES OCCURRED" << std::endl;
-    else
-        std::cout << "All tests succeeded" << std::endl;
-    return anyFailed ? 1 : 0;
+        *((long *) (size_t)rand()) = 0xDEADBEEF;
+        exit(-1);
+    }
+    std::cout << "All tests succeeded" << std::endl;
+    return 0;
 }
