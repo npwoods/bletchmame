eefdad392f4afd11b56ccc9bcfc5b89f85424b5d
npwoods <npwoods@mess.org>
2020-12-12 15:29:15 -0500

    Uninlined the various MameVersion methods

--- src/mameversion.h
+++ src/mameversion.h
@@ -21,50 +21,18 @@ public:
 	class Test;
 
 	// ctor
-	MameVersion(const QString &version)
-	{
-		parse(version, m_major, m_minor, m_dirty);
-	}
-
-	MameVersion(int major, int minor, bool dirty)
-	{
-		m_major = major;
-		m_minor = minor;
-		m_dirty = dirty;
-	}
+	MameVersion(const QString &version);
+	MameVersion(int major, int minor, bool dirty);
 
 	// accessors
 	int major() const	{ return m_major; }
 	int minor() const	{ return m_minor; }
 	bool dirty() const	{ return m_dirty; }
 
-	bool isAtLeast(const MameVersion &that) const
-	{
-		bool result;
-		if (major() > that.major())
-			result = true;
-		else if (major() < that.major())
-			result = false;
-		else if (minor() > that.minor())
-			result = true;
-		else if (minor() < that.minor())
-			result = false;
-		else
-			result = dirty() || !that.dirty();
-		return result;		
-	}
-
-	MameVersion nextCleanVersion() const
-	{
-		return MameVersion(major(), minor() + (dirty() ? 1 : 0), false);
-	}
-
-	QString toString() const
-	{
-		return QString(dirty() ? "%1.%2 (dirty)" : "%1.%2").arg(
-			QString::number(major()),
-			QString::number(minor()));
-	}
+	// methods
+	bool isAtLeast(const MameVersion &that) const;
+	MameVersion nextCleanVersion() const;
+	QString toString() const;
 
 private:
 	int	m_major;
