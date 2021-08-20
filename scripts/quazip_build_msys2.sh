3859da7fe81cda0932bc37563c1b6010c6923869
npwoods <npwoods@mess.org>
2021-08-19 22:19:09 -0400

    WIP

--- scripts/quazip_build_msys2.sh
+++ scripts/quazip_build_msys2.sh
@@ -15,8 +15,16 @@ QUAZIP_DIR=$(dirname $BASH_SOURCE)/../deps/quazip
 INSTALL_DIR=$(dirname $BASH_SOURCE)/../deps/msys2
 QUAZIP_BUILD_DIR=$(dirname $BASH_SOURCE)/../deps/build/msys2/quazip
 
-# Build and install it!
+# Clean directories
 rm -rf $QUAZIP_BUILD_DIR
+mkdir -p $QUAZIP_BUILD_DIR
+
+# Make directories absolute
+QUAZIP_DIR=$(realpath $QUAZIP_DIR)
+INSTALL_DIR=$(realpath $INSTALL_DIR)
+QUAZIP_BUILD_DIR=$(realpath $QUAZIP_BUILD_DIR)
+
+# Build and install it!
 cmake -S$QUAZIP_DIR -B$QUAZIP_BUILD_DIR -DBUILD_SHARED_LIBS=off -DQUAZIP_QT_MAJOR_VERSION=6 \
 	-DQt6_DIR=$INSTALL_DIR/lib/cmake/Qt6											\
 	-DQt6Core_DIR=$INSTALL_DIR/lib/cmake/Qt6Core									\
