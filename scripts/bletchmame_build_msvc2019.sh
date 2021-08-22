84d51a2cbd67908ee012893d99db13a479df7314
npwoods <npwoods@mess.org>
2021-08-22 11:39:04 -0400

    Defaulting Qt6 installation directory for MSVC2019 build scripts

--- scripts/bletchmame_build_msvc2019.sh
+++ scripts/bletchmame_build_msvc2019.sh
@@ -9,8 +9,11 @@ if [ -z "$BASH_SOURCE" ]; then
   echo "Null BASH_SOURCE"
   exit
 fi
-if [ -z "$QT_INSTALL_DIR" ]; then
-  echo "Null QT_INSTALL_DIR"
+if [ -z "$QT6_INSTALL_DIR" ]; then
+  QT6_INSTALL_DIR=/c/Qt/6.1.2/msvc2019_64
+fi
+if [ ! -d "$QT6_INSTALL_DIR" ]; then
+  echo "Bad QT6_INSTALL_DIR"
   exit
 fi
 
@@ -22,20 +25,20 @@ DEPS_INSTALL_DIR=$(realpath $(dirname $BASH_SOURCE)/../deps/msvc2019)
 
 # Set up build directory
 rm -rf ${BLETCHMAME_BUILD_DIR}
-cmake -S. -B${BLETCHMAME_BUILD_DIR}									\
-	-G"Visual Studio 16 2019"										\
-	-DQt6_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6							\
-	-DQt6Core_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6Core					\
-	-DQt6CoreTools_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6CoreTools		\
-	-DQt6GuiTools_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6GuiTools			\
-	-DQt6Test_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6Test					\
-	-DQt6Widgets_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6Widgets			\
-	-DQt6WidgetsTools_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6WidgetsTools	\
-	-DQuaZip-Qt6_DIR=$DEPS_INSTALL_DIR/lib/cmake/QuaZip-Qt6-1.1		\
-	-DQUAZIP_LIBRARIES=$DEPS_INSTALL_DIR/lib/quazip1-qt6d.lib		\
-	-DEXPAT_LIBRARY=$DEPS_INSTALL_DIR/lib/libexpatd.lib				\
-	-DEXPAT_INCLUDE_DIR=$DEPS_INSTALL_DIR/include					\
-	-DZLIB_LIBRARY=$DEPS_INSTALL_DIR/lib/zlibstaticd.lib			\
+cmake -S. -B${BLETCHMAME_BUILD_DIR}										\
+	-G"Visual Studio 16 2019"											\
+	-DQt6_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6							\
+	-DQt6Core_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6Core					\
+	-DQt6CoreTools_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6CoreTools			\
+	-DQt6GuiTools_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6GuiTools			\
+	-DQt6Test_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6Test					\
+	-DQt6Widgets_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6Widgets				\
+	-DQt6WidgetsTools_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6WidgetsTools	\
+	-DQuaZip-Qt6_DIR=$DEPS_INSTALL_DIR/lib/cmake/QuaZip-Qt6-1.1			\
+	-DQUAZIP_LIBRARIES=$DEPS_INSTALL_DIR/lib/quazip1-qt6d.lib			\
+	-DEXPAT_LIBRARY=$DEPS_INSTALL_DIR/lib/libexpatd.lib					\
+	-DEXPAT_INCLUDE_DIR=$DEPS_INSTALL_DIR/include						\
+	-DZLIB_LIBRARY=$DEPS_INSTALL_DIR/lib/zlibstaticd.lib				\
 	-DZLIB_INCLUDE_DIR=$DEPS_INSTALL_DIR/include
 cmake --build ${BLETCHMAME_BUILD_DIR} --parallel
 cmake --install ${BLETCHMAME_INSTALL_DIR} --prefix ${BLETCHMAME_INSTALL_DIR} --config Debug
--- scripts/quazip_build_msvc2019.sh
+++ scripts/quazip_build_msvc2019.sh
@@ -1,5 +1,5 @@
 ###################################################################################
-# quazip_build_msvc.sh - Builds QuaZip for MSVC (Debug)                           #
+# quazip_build_msvc.sh - Builds QuaZip for MSVC                                   #
 ###################################################################################
 
 # Sanity check
@@ -7,8 +7,11 @@ if [ -z "$BASH_SOURCE" ]; then
   echo "Null BASH_SOURCE"
   exit
 fi
-if [ -z "$QT_INSTALL_DIR" ]; then
-  echo "Null QT_INSTALL_DIR"
+if [ -z "$QT6_INSTALL_DIR" ]; then
+  QT6_INSTALL_DIR=/c/Qt/6.1.2/msvc2019_64
+fi
+if [ ! -d "$QT6_INSTALL_DIR" ]; then
+  echo "Bad QT6_INSTALL_DIR"
   exit
 fi
 
@@ -23,9 +26,9 @@ cmake -S$QUAZIP_DIR -B$QUAZIP_BUILD_DIR -DQUAZIP_QT_MAJOR_VERSION=6	\
 	-DCMAKE_BUILD_TYPE=Debug										\
 	--install-prefix $INSTALL_DIR									\
 	-G"Visual Studio 16 2019"										\
-	-DQt6_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6							\
-	-DQt6Core_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6Core					\
-	-DQt6CoreTools_DIR=$QT_INSTALL_DIR/lib/cmake/Qt6CoreTools		\
+	-DQt6_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6						\
+	-DQt6Core_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6Core				\
+	-DQt6CoreTools_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6CoreTools		\
 	-DZLIB_LIBRARY=$INSTALL_DIR/lib/zlibstaticd.lib					\
 	-DZLIB_INCLUDE_DIR=$INSTALL_DIR/include
 
