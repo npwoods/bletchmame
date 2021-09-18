056bce7b43e5494f1ddeeb67569d36703ac1498e
npwoods <npwoods@mess.org>
2021-09-18 19:10:21 -0400

    Supporting Clang-CL (under MSVC2019)

--- scripts/bletchmame_build_msvc2019.sh
+++ scripts/bletchmame_build_msvc2019.sh
@@ -4,7 +4,7 @@
 # bletchmame_build_msys2.sh - Builds BletchMAME under MSVC                        #
 ###################################################################################
 
-# Sanity check
+# sanity check
 if [ -z "$BASH_SOURCE" ]; then
   echo "Null BASH_SOURCE"
   exit
@@ -17,16 +17,29 @@ if [ ! -d "$QT6_INSTALL_DIR" ]; then
   exit
 fi
 
-# Identify directories
+# identify directories
 BLETCHMAME_DIR=$(dirname $BASH_SOURCE)/..
 BLETCHMAME_BUILD_DIR=build/msvc2019
 BLETCHMAME_INSTALL_DIR=${BLETCHMAME_BUILD_DIR}
 DEPS_INSTALL_DIR=$(realpath $(dirname $BASH_SOURCE)/../deps/msvc2019)
 
+# toolset is v142 (MSVC2019), but can also be ClangCL
+TOOLSET=v142
+
+# parse arguments
+while getopts "t:" OPTION; do
+   case $OPTION in
+      t)
+         TOOLSET=$OPTARG
+		 ;;
+   esac
+done
+
 # Set up build directory
 rm -rf ${BLETCHMAME_BUILD_DIR}
 cmake -S. -B${BLETCHMAME_BUILD_DIR}										\
 	-G"Visual Studio 16 2019"											\
+	-T"$TOOLSET"														\
 	-DQt6_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6							\
 	-DQt6Core_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6Core					\
 	-DQt6CoreTools_DIR=$QT6_INSTALL_DIR/lib/cmake/Qt6CoreTools			\
