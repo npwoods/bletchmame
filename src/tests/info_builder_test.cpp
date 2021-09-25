21335c3c4c4b75e7d8b15bcbe4cf45b52441fcf1
npwoods <npwoods@mess.org>
2021-09-25 10:14:30 -0400

    Adding tests to ensure that info DB database builds are byte-for-byte identical (until we rev the format, of course)

--- src/tests/info_builder_test.cpp
+++ src/tests/info_builder_test.cpp
@@ -19,6 +19,11 @@ namespace
 
     private slots:
         void general();
+		void compareBinaries_coco()		{ compareBinaries(":/resources/listxml_coco.xml"); }
+		void compareBinaries_alienar()	{ compareBinaries(":/resources/listxml_alienar.xml"); }
+
+	private:
+		void compareBinaries(const QString &fileName);
     };
 }
 
@@ -94,6 +99,68 @@ void Test::general()
 	QVERIFY(byteArray.size() > 0);
 }
 
+
+//-------------------------------------------------
+//  compareBinaries - perform a binary comparison
+//	to validate that byte for byte, the info DB we
+//	build is identical to whats there
+// 
+//	a caveat - info DB is (for now) endian sensitive,
+//  but in practice all of the platforms we care
+//	about are little endian
+//-------------------------------------------------
+
+void Test::compareBinaries(const QString &fileName)
+{
+	const bool dumpBinaries = false;
+
+	// get the binaries
+	QByteArray byteArray = buildInfoDatabase(fileName);
+	QVERIFY(byteArray.size() > 0);
+
+	// get the binary file name
+	QString binFileName = fileName;
+	binFileName.replace(".xml", ".bin");
+	QVERIFY(binFileName != fileName);
+
+	// dump the binaries if appropriate
+	if (dumpBinaries)
+	{
+		int lastSlashPos = binFileName.lastIndexOf('/');
+		QVERIFY(lastSlashPos >= 0);
+		QString dumpBinFileName = binFileName.right(binFileName.size() - lastSlashPos - 1);
+
+		QFile dumpFile(dumpBinFileName);
+		QVERIFY(dumpFile.open(QIODevice::WriteOnly));
+		dumpFile.write(byteArray);
+	}
+
+	// read the binary file
+	QFile binFile(binFileName);
+	QVERIFY(binFile.open(QIODevice::ReadOnly));
+	QByteArray binByteArray = binFile.readAll();
+	binFile.close();
+
+	// the moment of truth - is the binary that we built byte-for-byte identical to what is there?
+	QVERIFY(byteArray.size() == binByteArray.size());
+	for (size_t i = 0; i < byteArray.size(); i++)
+	{
+		if (byteArray[i] != binByteArray[i])
+		{
+			char buffer[256];
+			snprintf(buffer, std::size(buffer), "Difference at index #%d (0x%02X vs 0x%02X)",
+				(int)i,
+				(unsigned)(std::uint8_t)byteArray[i],
+				(unsigned)(std::uint8_t)binByteArray[i]);
+			QWARN(buffer);
+			break;
+		}
+	}
+	QVERIFY(byteArray == binByteArray);
+}
+
+
+//-------------------------------------------------
 
 static TestFixture<Test> fixture;
 #include "info_builder_test.moc"
