0e7b92fe7eccfe6e386ef327e732b16b7bcb20fd
npwoods <npwoods@mess.org>
2022-02-16 18:35:41 -0500

    Added a general unit test for ImportMameIniJob

--- src/tests/importmameinijob_test.cpp
+++ src/tests/importmameinijob_test.cpp
@@ -15,6 +15,7 @@ class ImportMameIniJob::Test : public QObject
 	Q_OBJECT
 
 private slots:
+	void general();
 	void areFileInfosEquivalent();
 };
 
@@ -22,6 +23,55 @@ private slots:
 //**************************************************************************
 //  IMPLEMENTATION
 //**************************************************************************
+
+//-------------------------------------------------
+//  general
+//-------------------------------------------------
+
+void ImportMameIniJob::Test::general()
+{
+	// create a temporary directory
+	QTemporaryDir tempDirObj;
+	QVERIFY(tempDirObj.isValid());
+	QDir tempDir(tempDirObj.path());
+
+	// copy our test MAME.INI to the temporary directory
+	QString mameIniPath = tempDir.filePath("mame.ini");
+	QVERIFY(QFile::copy(":/resources/testmameini.ini", mameIniPath));
+
+	// set up preferences
+	Preferences prefs;
+	prefs.setGlobalPath(Preferences::global_path_type::ROMS,	tempDir.filePath("./mamefiles/roms1"));
+	prefs.setGlobalPath(Preferences::global_path_type::SAMPLES, tempDir.filePath("./mamefiles/samples"));
+
+	// set up an import job and import our INI
+	ImportMameIniJob importJob(prefs);
+	QVERIFY(importJob.loadMameIni(mameIniPath));
+
+	// and validate the results
+	QVERIFY(importJob.entries().size() == 10);
+	QVERIFY(importJob.entries()[0]->labelDisplayText()	== "ROMs");
+	QVERIFY(importJob.entries()[0]->importAction()		== ImportMameIniJob::ImportAction::AlreadyPresent);
+	QVERIFY(importJob.entries()[1]->labelDisplayText()	== "ROMs");
+	QVERIFY(importJob.entries()[1]->importAction()		== ImportMameIniJob::ImportAction::Supplement);
+	QVERIFY(importJob.entries()[2]->labelDisplayText()	== "Samples");
+	QVERIFY(importJob.entries()[2]->importAction()		== ImportMameIniJob::ImportAction::AlreadyPresent);
+	QVERIFY(importJob.entries()[3]->labelDisplayText()	== "Config Files");
+	QVERIFY(importJob.entries()[3]->importAction()		== ImportMameIniJob::ImportAction::Replace);
+	QVERIFY(importJob.entries()[4]->labelDisplayText()	== "NVRAM Files");
+	QVERIFY(importJob.entries()[4]->importAction()		== ImportMameIniJob::ImportAction::Replace);
+	QVERIFY(importJob.entries()[5]->labelDisplayText()	== "CHD Diff Files");
+	QVERIFY(importJob.entries()[5]->importAction()		== ImportMameIniJob::ImportAction::Replace);
+	QVERIFY(importJob.entries()[6]->labelDisplayText()	== "Hash Files");
+	QVERIFY(importJob.entries()[6]->importAction()		== ImportMameIniJob::ImportAction::Supplement);
+	QVERIFY(importJob.entries()[7]->labelDisplayText()	== "Artwork Files");
+	QVERIFY(importJob.entries()[7]->importAction()		== ImportMameIniJob::ImportAction::Supplement);
+	QVERIFY(importJob.entries()[8]->labelDisplayText()	== "Plugins");
+	QVERIFY(importJob.entries()[8]->importAction()		== ImportMameIniJob::ImportAction::Supplement);
+	QVERIFY(importJob.entries()[9]->labelDisplayText()	== "Cheats");
+	QVERIFY(importJob.entries()[9]->importAction()		== ImportMameIniJob::ImportAction::Supplement);
+}
+
 
 //-------------------------------------------------
 //  areFileInfosEquivalent
