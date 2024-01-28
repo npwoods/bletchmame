033e5d2ebe3063fcc8927010a6c99f2b951e3c70
npwoods <npwoods@mess.org>
2024-01-28 15:19:45 -0500

    Update bletchmame_build_msys2.sh

--- scripts/bletchmame_build_msys2.sh
+++ scripts/bletchmame_build_msys2.sh
@@ -40,8 +40,6 @@ cmake -S. -B${BLETCHMAME_BUILD_DIR}								\
 	-DUSE_PROFILER=${USE_PROFILER}								\
 	-DCMAKE_BUILD_TYPE=${BUILD_TYPE}							\
 	-DCMAKE_PREFIX_PATH="${DEPS_INSTALL_DIR};${QT_INSTALL_DIR}"	\
-	-DCMAKE_DISABLE_FIND_PACKAGE_harfbuzz=TRUE					\
-	--debug-find-pkg=WrapPNG -DQT_DEBUG_FIND_PACKAGE=ON
 
 # generate version.gen.h
 mkdir -p ${BLETCHMAME_DIR}/include
